9db38cfa38b72d343598122e90d11978485e58e2
joker-jonesy <spashmcgrath94@yahoo.com>
2022-10-05 15:06:17 -0400

    Second Kermit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
-
 function App() {
   return (
     <div className="App">
-
+      <nav>
+          <a href="#">HOME</a>
+          <a href="#">About</a>
+      </nav>
+        <div className="box">1</div>
     </div>
   );
 }
